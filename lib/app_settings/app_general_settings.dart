fcaba1ed674bc0acfded57fb6aa190eff24b89f0
Matias Espina <matias@devbite.de>
2024-07-07 19:02:32 +0200

    .

--- lib/app_settings/app_general_settings.dart
+++ lib/app_settings/app_general_settings.dart
@@ -2,6 +2,10 @@ class AppGeneralSettings {
   static const bool useTopAppBar = false;
   static const bool useFloatingSpeedDialMenu = true;
   static const bool useAppDrawerMenu = false;
+  static const String minimumiOSVersion =
+      '13.0'; // Set this manually in Xcode & Podfile
+  static const String minimumAndroidVersion =
+      '23'; // Set this manually in AndroidManifest.xml
 }
 
 class DebugConfig {
